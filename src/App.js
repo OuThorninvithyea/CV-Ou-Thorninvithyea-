a5611e48f425fadb6277dc9f8640e91cb979fd5b
Shawnnnnhh <100029619+Shawnnnnhh@users.noreply.github.com>
2025-09-19 17:41:39 +0700

    add analys

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Analytics } from "@vercel/analytics/react";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Experience from "./components/Experience";
@@ -19,6 +20,7 @@ function App() {
       <Projects />
       <Education />
       <Contact />
+      <Analytics />
     </div>
   );
 }
